9f50778dd91f6b6781b04a44ddf6d727dff5cf8f
amaury <tissot.amaury@gmail.com>
2022-07-02 17:57:53 +0200

    Add get API data function

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ specifications :
 architecture : 
 - [*] write a function with a simple form validation;
 - [] write a function to convert celsius into fahrenheit;
-- [] write a function that hit the API. This function must take the location name and return the weather data location;
+- [*] write a function that hit the API. This function must take the location name and return the weather data location;
 - [] write a function that process the JSON data send by the API and return a object with only the data needed;
 - [] write a function that display the weather info on the webpage;
 - [] write a function that display a loading component until the API responded;
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 
+let API_keys = '3869021086b53413c374a899138b0dfa';
+
+
+let inputValue;
 
 // function to get form input value
 function formValidation () {
@@ -6,6 +10,34 @@ function formValidation () {
     formE.addEventListener('submit', (event) => {
         event.preventDefault();
         inputValue = document.querySelector('input').value
+        console.log(inputValue)
+
+        // maybe the inputValue need to be process (toLowerCase ?)
     })
 };
 
+// async function to fetch API
+
+async function getAPI (location){
+    try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_keys}&units=metric`, {mode: 'cors'});
+        const data = await response.json()
+        console.table(data);
+        return data
+        
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+getAPI('Euzet')
+
+
+
+
+
+
+
+
+
+formValidation();
